5cafa14d3bb6e3ee26249916fb23138ac1c7d9de
yarivShamash <yariv.shamash@gmail.com>
2024-06-24 13:49:14 +0300

    server init

---
+++ server/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2SUhCQo581JY_1wyNgxREh-o6xmKlrRI",
+  authDomain: "the-left-or-right-game.firebaseapp.com",
+  projectId: "the-left-or-right-game",
+  storageBucket: "the-left-or-right-game.appspot.com",
+  messagingSenderId: "168501397133",
+  appId: "1:168501397133:web:eaf80ebe74c15301e9c4c4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
