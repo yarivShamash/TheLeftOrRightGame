3affb63ff1e062e544cd1565aa728eb80fd0cb78
yarivShamash <yariv.shamash@gmail.com>
2024-06-26 15:23:17 +0300

    trying

--- client/src/axiosconfig.ts
+++ client/src/axiosconfig.ts
@@ -1,7 +1,16 @@
 import axios from "axios";
+import { SERVER_LOCAL_PORT } from "./consts";
+
+// const isDevMode = process.env.NODE_ENV === "development";
+const firebaseHostingUrl = process.env.FIREBASE_HOSTING_URL;
 
 const api = axios.create({
-  baseURL: "the-left-or-right-game--pr5-firebase-hosting-0suikrc0.web.app/api",
+  baseURL: firebaseHostingUrl
+    ? firebaseHostingUrl + "/api"
+    : `http://localhost:${SERVER_LOCAL_PORT}`,
 });
+// baseURL: isDevMode
+//   ? `http://localhost:${SERVER_LOCAL_PORT}`
+//   : "the-left-or-right-game--pr5-firebase-hosting-0suikrc0.web.app/api",
 
 export default api;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,29 +1,21 @@
-import { FormEventHandler, useState } from "react";
-// import { useNavigate } from "@tanstack/react-router";
+import { FormEventHandler } from "react";
+import { useNavigate } from "@tanstack/react-router";
 
 import { FormContainer, Input, LoadingTitle, SubmitButton } from "./styles";
 import { UserForm } from "./types";
 import { useUser } from "../../providers/UserProvider";
 
 export const Home = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { loading, getUser } = useUser();
-  const [userName, setUserName] = useState("");
-
-  const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(e.target.value);
-  };
 
   const handleSubmit: FormEventHandler<UserForm> = async (e) => {
     e.preventDefault();
-    // @ts-expect-error This is expected due to the structure of the event object.
-    const userName = e.target.name.value;
-
-    if (!userName) return;
 
     try {
-      await getUser(userName);
-      // navigate({ to: "/game" });
+      // @ts-expect-error This is expected due to the structure of the event object.
+      await getUser(e.target.name.value);
+      navigate({ to: "/game" });
     } catch (error) {
       console.error("error getting user:", error);
     }
@@ -35,14 +27,8 @@ export const Home = () => {
         <LoadingTitle>LOADING..</LoadingTitle>
       ) : (
         <>
-          <Input
-            placeholder="Hey, what's your name?"
-            type="text"
-            name="name"
-            value={userName}
-            onChange={handleUserNameChange}
-          />
-          <SubmitButton type="submit" value="START" disabled={!userName} />
+          <Input placeholder="Hey, what's your name?" type="text" name="name" />
+          <SubmitButton type="submit" value="START" />
         </>
       )}
     </FormContainer>
