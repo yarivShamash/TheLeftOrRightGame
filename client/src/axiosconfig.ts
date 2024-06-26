3dbc60e45338a5682fef85fe0c1cdceea9bb3f05
yarivShamash <yariv.shamash@gmail.com>
2024-06-26 15:05:19 +0300

    trying something

--- client/src/axiosconfig.ts
+++ client/src/axiosconfig.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
-import { SERVER_LOCAL_PORT } from "./consts";
-
-const isDevMode = process.env.NODE_ENV === "development";
 
 const api = axios.create({
-  baseURL: isDevMode
-    ? `http://localhost:${SERVER_LOCAL_PORT}`
-    : "https://the-left-or-right-game.web.app/api",
+  baseURL: "the-left-or-right-game--pr5-firebase-hosting-0suikrc0.web.app/api",
 });
 
 export default api;
