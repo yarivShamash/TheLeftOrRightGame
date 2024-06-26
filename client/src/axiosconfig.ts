848f868bc693c12f62bb3779fe2b9d70ffc6bb47
yarivShamash <yariv.shamash@gmail.com>
2024-06-26 14:58:47 +0300

    Server and client communicate over the same url

--- client/src/axiosconfig.ts
+++ client/src/axiosconfig.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { SERVER_LOCAL_PORT } from "./consts";
 
+const isDevMode = process.env.NODE_ENV === "development";
+
 const api = axios.create({
-  baseURL: `http://localhost:${SERVER_LOCAL_PORT}`,
+  baseURL: isDevMode
+    ? `http://localhost:${SERVER_LOCAL_PORT}`
+    : "https://the-left-or-right-game.web.app/api",
 });
 
 export default api;
